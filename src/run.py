7943e8b8cdcdbee8aeb0f967dde3de8815b7ffc7
Amanda <amanda_bruncrona@hotmail.com>
2025-12-12 11:55:59 +0200

    Corrected get_model_input import and function call

--- src/run.py
+++ src/run.py
@@ -5,11 +5,10 @@ import gzip
 import shutil
 import logging
 import argparse
-
 import torch
-import numpy as np
 
-from karhu.utils_input import get_model_input, scale_model_input, descale_minmax
+from karhu.utils_input import scale_model_input, descale_minmax
+from karhu.utils_helena import get_model_input
 from karhu.models import CNN_gmax
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -23,26 +22,17 @@ def main(args: argparse.Namespace):
     Args:
         args (argparse.Namespace): Namespace containing the configuration parameters.
     """
-
-    n_profile_points = 64
-    x_1 = np.linspace(1e-5, 1, n_profile_points) ** (1 / 4)
-
     run_dir = args.helena_directory
     model, scaling_params = load_model(model_dir=args.model_directory)
 
-    # TODO: We are assuming that fort.12 is gzipped already..., should check 
     if not os.path.exists(os.path.join(run_dir, "fort.12")):
+        if not os.path.exists(os.path.join(run_dir, "fort.12.gz")):
+            raise FileNotFoundError(f"Neither fort.12 nor fort.12.gz found in {run_dir}")
         with gzip.open(os.path.join(run_dir, 'fort.12.gz'), 'rb') as f_in:
             with open(os.path.join(run_dir, 'fort.12'), 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-    x = get_model_input(
-        filename_f10=os.path.join(run_dir, 'fort.10'),
-        filename_f12=os.path.join(run_dir, 'fort.12'),
-        filename_f20=os.path.join(run_dir, 'fort.20'),
-        x_1=x_1,
-        n_profile_points=n_profile_points,)
-
+    x = get_model_input(heldir=run_dir)
     x = scale_model_input(x, scaling_params)
     y_pred = model(*x)
     y_pred = descale_minmax(
