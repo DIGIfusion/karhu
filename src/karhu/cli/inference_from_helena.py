164044a87ccb8b666e8c9c2e8f19edffbb26138d
Amanda <amanda_bruncrona@hotmail.com>
2026-01-19 14:51:39 +0200

    Plasma shape in polar coordinates (#10)
    * Duplicate code has been removed
    
    * Added common functions for boundary conversion
    
    * Flexibility in model class to account for architecture changes in upcoming models.
    
    * Changed function name from get_model_input to load_from_helena function and now returns a list of tensors instead of a list of numpy arrays.
    
    * Removed run.py since cli/inference_from_helena.py contains the same information.
    
    * Defined imports in init so that the classes and functions are available when importing karhu from an external code.
    
    * Updated models for polar coordinate boundary

--- src/karhu/cli/inference_from_helena.py
+++ src/karhu/cli/inference_from_helena.py
@@ -2,28 +2,19 @@
 An example python script for running KARHU from a HELENA directory and writing the result to a file. 
 """
 import argparse 
-from karhu.utils_helena import get_model_input
+from karhu.utils_helena import load_from_helena
 from karhu.models import load_model
 from karhu.utils_input import scale_model_input, scale_model_output
 
 import torch
 
-WP     = torch.float32 #TODO: will this change in future? 
+WP     = torch.float32  # TODO: will this change in future? 
 DEVICE = 'cuda' if torch.cuda.is_available() else "cpu"
     
-def main(model_dir:str, helena_dir: str):
+def main(model_dir: str, helena_dir: str):
     model, scaling_params = load_model(model_dir)
     model = model.to(device=DEVICE, dtype=WP)
-    p, q, rbphi, vy, bmag, rmag = get_model_input(helena_dir)
-
-    x = [torch.tensor(p, dtype=WP).unsqueeze(0).unsqueeze(0), 
-        torch.tensor(q, dtype=WP).unsqueeze(0).unsqueeze(0), 
-        torch.tensor(rbphi, dtype=WP).unsqueeze(0).unsqueeze(0), 
-        torch.tensor(vy, dtype=WP).unsqueeze(0).unsqueeze(0), 
-        torch.tensor(bmag, dtype=WP).unsqueeze(0), 
-        torch.tensor(rmag, dtype=WP).unsqueeze(0), 
-        ]
-    print(x)
+    x = load_from_helena(helena_dir)
     x = scale_model_input(x, scaling_params)
     with torch.no_grad(): 
         y = model(*x)
