bb6166836e837e1ddc59e2e08a8f4394556eeb6f
Amanda <amanda_bruncrona@hotmail.com>
2026-04-15 12:11:55 +0300

    Update inference_from_jintrac.py

--- src/karhu/cli/inference_from_jintrac.py
+++ src/karhu/cli/inference_from_jintrac.py
@@ -4,7 +4,9 @@ An example python script for running KARHU from a HELENA directory and writing t
 import argparse
 import sys
 import numpy as np
-from enum import Enum 
+from enum import Enum
+import torch
+
 
 sys.path.append("/home/mn2596/JETPEDESTAL_ANALYSIS/karhu/src/karhu")
 from karhu.utils_helena import load_from_helena
@@ -12,8 +14,6 @@ from karhu.models import load_model
 from karhu.utils_input import scale_model_input, scale_model_output
 from karhu.common import convert_profiles_si_to_dimensionless, get_polar_from_rz
 from karhu.utils_input import interpolate_profile
-
-import torch
 
 
 """
@@ -63,9 +63,9 @@ def get_from_datfiles(datfiles_dir: str, model_config: dict):
     pr_karhu = interpolate_profile(psig, pr, model_config["karhu_psin_axis"])
     q_karhu = interpolate_profile(psig, q, model_config["karhu_psin_axis"])
     rbphi_karhu = interpolate_profile(psig, rbphi, model_config["karhu_psin_axis"])
-    symmetric = False 
+    symmetric = False
     rhobndry, thetabdry = get_polar_from_rz(
-        r_vals=rbnd, z_vals=zbnd,  symmetric=symmetric)
+        r_vals=rbnd, z_vals=zbnd, symmetric=symmetric)
 
     rhobndry_karhu = interpolate_profile(
         x_0=thetabdry, y_0=rhobndry, x_1=model_config["karhu_theta_axis"])
@@ -76,14 +76,14 @@ def get_from_datfiles(datfiles_dir: str, model_config: dict):
          torch.tensor(rhobndry_karhu, dtype=torch.float32).unsqueeze(0).unsqueeze(0),
          torch.tensor(B_mag, dtype=torch.float32).unsqueeze(0),
          torch.tensor(R_mag, dtype=torch.float32).unsqueeze(0),
-    ]
-    return x 
+         ]
+    return x
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("Run KARHU on a HELENA directory")
     parser.add_argument("-dd", "--data_directory", type=str, required=True, help="Path to data directory")
-    parser.add_argument("-m",  "--model_directory", type=str, required=True, help="Path to KARHU model directory")
+    parser.add_argument("-m", "--model_directory", type=str, required=True, help="Path to KARHU model directory")
     parser.add_argument('-w', "--write_filename", type=str, default=None, help="Write the prediction to file with name given here, if not passed, no file will be written")
     parser.add_argument("-rm", "--runmode", type=int, choices=[mode.value for mode in RUNMODE], default=RUNMODE.HELENA.value, help="Where to source the input data from, 0 for HELENA, 1 for datfiles")
     args = parser.parse_args()
@@ -94,12 +94,11 @@ if __name__ == "__main__":
 
     if args.runmode == RUNMODE.HELENA.value:
         x = load_from_helena(args.data_directory,
-                             karhu_psin_axis=model_config["karhu_psin_axis"], 
+                             karhu_psin_axis=model_config["karhu_psin_axis"],
                              karhu_theta_axis=model_config["karhu_theta_axis"])
 
     elif args.runmode == RUNMODE.DATFILES.value:
-        x = get_from_datfiles(
-            args.model_directory, args.data_directory, model_config)
+        x = get_from_datfiles(args.data_directory, model_config)
     else:
         raise NotImplementedError(
             "Choose (0, or 1) for runmode, got {}".format(args.runmode))
